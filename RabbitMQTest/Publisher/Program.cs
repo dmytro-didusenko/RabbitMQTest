d13a2f4e19c5342b79ca3c5719256320afff5bbb
Dmytro Didusenko <d.didusenko@intetics.com>
2021-11-30 00:16:04 +0200

    Added Publisher and Receiver projects. Created connection to message broker server

---
+++ RabbitMQTest/Publisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using Domain.Entities;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "dev-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var messageBody = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(GetUserData()));
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "dev-queue",
+                basicProperties: null,
+                body: messageBody);
+
+            Console.WriteLine("Your message was sent");
+
+            Console.ReadLine();
+        }
+
+        public static User GetUserData()
+        {
+            var user = new User();
+            
+            Console.Write("Enter User's name: ");
+            user.Name = Console.ReadLine();
+            Console.Write("Enter User's age: ");
+            user.Age = Convert.ToInt32(Console.ReadLine());
+
+            return user;
+        }
+    }
+}
---
+++ RabbitMQTest/Receiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+using Domain.Entities;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "dev-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (User, us) =>
+            {
+                var user = us.Body.ToArray();
+                var message = JsonSerializer.Deserialize<User>(Encoding.UTF8.GetString(user));
+                Console.WriteLine($"User name is {message.Name} and he has {message.Age} years old");
+            };
+
+            channel.BasicConsume(
+                queue: "dev-queue",
+                autoAck: true,
+                consumer: consumer);
+
+            Console.ReadLine();
+
+        }
+    }
+}
